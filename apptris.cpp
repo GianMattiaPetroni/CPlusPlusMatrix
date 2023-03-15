944bd2d5455b4354798f2a6bd6a55c42c10b565c
GianMattiaPetroni <petronigianmattia@gmail.com>
2023-03-15 17:51:10 +0100

    tris funzionante

--- apptris.cpp
+++ apptris.cpp
@@ -2,9 +2,6 @@
 
 using namespace std;
 
-// 1 per la vittoria
-// 0 per continuare la partita
-// -1 per il pareggio
 int isWinner(char *arr)
 {
 
@@ -59,9 +56,7 @@ void stampa(char *a)
 
 int main()
 {
-
-    char vuoto = '_';
-
+    char vuoto = '-';
     // inizializzo la tabella
     char tabella[9];
     for (int i = 0; i < 9; i++)
@@ -69,29 +64,23 @@ int main()
         tabella[i] = vuoto;
     }
     stampa(tabella);
-
     bool xIsNext = true; // gestisce il turno
     while (true)         // i turni del gioco
     {
-
         int mossa;
-
         cout << "Giocatore " << (xIsNext ? 'X' : 'O') << " inserisci la tua mossa (1-9) ";
         cin >> mossa;
-        mossa--; // per capirci ;)
-
+        mossa--;                                                        // per capirci ;)
         while (tabella[mossa] != vuoto || (!(0 <= mossa && mossa < 9))) // controllo 2 condizioni (valido e vuoto)
         {
             cout << "Non puoi inserire qui" << endl;
             cout << "Giocatore " << (xIsNext ? 'X' : 'O') << " inserisci la tua mossa (1-9) ";
             cin >> mossa;
+            mossa--;
         }
-
         // se sono arrivato qui è solo perchè l'utente ha inserito un numero corretto
         xIsNext ? tabella[mossa] = 'X' : tabella[mossa] = 'O'; // se è vero metti X altrimenti metti O
-
         stampa(tabella);
-
         if (isWinner(tabella) == 1)
         {
             cout << "Hai vinto " << (xIsNext ? 'X' : 'O') << endl;
